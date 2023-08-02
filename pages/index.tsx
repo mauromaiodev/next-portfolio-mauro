881c350bb8d1a62929ee032cd82b42d522715104
Mauro <mauromaio.dev@gmail.com>
2023-08-02 14:47:04 -0300

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import dynamic from 'next/dynamic';
+import Contact from '../components/ContactUs';
+import SEO from '../components/SEO';
+import { openSource, showContactUs } from '../portfolio';
+import { GithubUserType } from '../types';
 const Navigation = dynamic(() => import('../components/Navigation'));
 const Greetings = dynamic(() => import('../containers/Greetings'));
 const Skills = dynamic(() => import('../containers/Skills'));
-const Proficiency = dynamic(() => import('../containers/Proficiency'));
 const Education = dynamic(() => import('../containers/Education'));
 const Experience = dynamic(() => import('../containers/Experience'));
 const Projects = dynamic(() => import('../containers/Projects'));
@@ -10,10 +13,6 @@ const Feedbacks = dynamic(() => import('../containers/Feedbacks'));
 const GithubProfileCard = dynamic(
   () => import('../components/GithubProfileCard')
 );
-import { openSource, showContactUs } from '../portfolio';
-import SEO from '../components/SEO';
-import Contact from '../components/ContactUs';
-import { GithubUserType } from '../types';
 
 export default function Home({
   githubProfileData,
@@ -26,7 +25,6 @@ export default function Home({
       <Navigation />
       <Greetings />
       <Skills />
-      <Proficiency />
       <Education />
       <Experience />
       <Feedbacks />
