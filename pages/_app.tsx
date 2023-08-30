74318ad943c2c0bbd06ab51ff8781eb4a8ee5866
Mauro <mauromaio.dev@gmail.com>
2023-08-30 17:18:27 -0300

    vercel-analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
-import { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { AppProps } from 'next/app';
 import '../styles/argon-design-system-react.css';
 import '../styles/styles.css';
 import '../styles/vendor/font-awesome/css/font-awesome.min.css';
 import '../styles/vendor/nucleo/css/nucleo.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  <Analytics />;
   return <Component {...pageProps} />;
 }
 
